nmalkin/janus

commit 5c740cb9e70ba3bde05bd8ec56ad565409c3b678

Switch to SQLite for storage
Makes it easier to aggregate and query for the information we will want.

server/lib/store.js
@@ -1,31 +1,37 @@
 'use strict';
 
-var redis = require('redis');
-
-var client = redis.createClient();
-
-client.on('error', function(err) {
-    console.log('Redis error:', err);
-});
-
-var logError = function(err) {
-    if(err) console.log('Redis error:', err);
-};
+var config = require('../lib/config');
+var sqlite = require('sqlite3');
+var db = new sqlite.Database(config.database);
 
 exports.getAlias = function(alias, callback) {
-    client.get('alias:' + alias, callback);
+    db.get('SELECT email FROM aliases WHERE alias=?', alias, function(err, row) {
+        if(err) callback(err);
+        else {
+            if(row === undefined) {
+                callback(null, null);
+            } else {
+                callback(null, row.email);
+            }
+        }
+    });
 };
 
 exports.getAliases = function(email, callback) {
-    client.lrange('aliases:' + email, 0, -1, callback);
+    db.all('SELECT alias FROM aliases WHERE email=?', email, function(err, rows) {
+        if(err) callback(err);
+        else {
+            callback(null, rows.map(function(row) {
+                return row.alias;
+            }));
+        }
+    });
 };
 
 exports.setAlias = function(realEmail, alias) {
-    client.set('alias:' + alias, realEmail, logError);
-    client.lpush('aliases:' + realEmail, alias, logError);
+    db.run('INSERT INTO aliases VALUES(?,?,1,"",0,"")', alias, realEmail);
 };
 
 exports.deleteAlias = function(realEmail, alias) {
-    client.del('alias:' + alias, logError);
-    client.lrem('aliases:' + realEmail, 1, alias, logError);
+    db.run('DELETE FROM aliases WHERE alias=?', alias);
 };

server/config/config.json.dist
@@ -5,6 +5,7 @@
     "smtpPort": 25,
     "audience": "https://janus.im",
     "domain": "janus.im",
+    "database": "var/janus.db",
     "send": {
         "port": 465,
         "host": "smtp.gmail.com",

server/scripts/db-setup
@@ -1,0 +1,10 @@
+#!/usr/local/bin/node
+
+var config = require('../lib/config');
+var sqlite = require('sqlite3').verbose();
+var db = new sqlite.Database(config.database);
+
+db.run('CREATE TABLE IF NOT EXISTS aliases (alias VARCHAR(64), email VARCHAR(128), status TINYINT, realm VARCHAR(128), forwards INTEGER, comment TEXT)');
+
+
+db.close();

commit aebe00c5bab37d95b730adbbe4602efad7c2a97a

Restore reverse-chronological order to alias display
Regression introduced in 5c740cb

server/lib/store.js
@@ -26,7 +26,7 @@ exports.getAlias = function(alias, callback) {
 
 /* Calls callback with list of active aliases for given email address. */
 exports.getAliases = function(email, callback) {
-    db.all('SELECT alias FROM aliases WHERE email=? AND status=1', email, function(err, rows) {
+    db.all('SELECT alias FROM aliases WHERE email=? AND status=1 ORDER BY rowid DESC', email, function(err, rows) {
         if(err) callback(err);
         else {
             callback(null, rows.map(function(row) {